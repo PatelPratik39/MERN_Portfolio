46a207331b95d5c8c46433e778de4a2d6a35105d
PatelPratik39 <ptl.pratik717@gmail.com>
2024-11-10 15:59:40 -0600

    Home Component is ready,  insatall typewrite effect package

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import Resume from '../assets/docs/Pratik_Patel_resume1.pdf'
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <div>
-        <h1>Home</h1>
+      <div className="container-fluid home-container">
+        <div className="container home-content">
+          <h1>Hi, I'm a </h1>
+          {/* Using Typewrite-effect pakage: npm i typewriter-effect */}
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "Full Stack Developer",
+                  "Software Engineer",
+                  "AWS Cloud Practioner"
+                ],
+                autoStart: true,
+                loop: true
+              }}
+            />
+          </h2>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me</button>
+            {/* <button className="btn btn-cv">My Resume </button> */}
+            <a className="btn btn-cv" href={Resume} download={"Pratik_Patel_Resume.pdf"}>My Resume </a>
+          </div>
+        </div>
       </div>
     </>
   );
