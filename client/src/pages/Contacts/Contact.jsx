a4a54ce9feeb36cae9c50356e782436b5373de56
PatelPratik39 <ptl.pratik717@gmail.com>
2024-11-11 23:40:48 -0600

    Annimation added

--- client/src/pages/Contacts/Contact.jsx
+++ client/src/pages/Contacts/Contact.jsx
@@ -1,4 +1,5 @@
-// import React from "react";
+import Rotate from "react-reveal/Rotate";
+import LightSpeed from "react-reveal/LightSpeed";
 import "./contact.css";
 import { BsLinkedin, BsGithub, BsInstagram, BsMedium } from "react-icons/bs";
 
@@ -11,62 +12,74 @@ const Contact = () => {
             <div className="col-md-6 col-lg-6 col-xk-6 col-sm-12">
               <div className="card1">
                 <div className="row border-line">
-                  <img
-                    src="https://img.freepik.com/free-vector/contact-us-button_78370-3753.jpg"
-                    alt="contact"
-                  />
+                  <LightSpeed>
+                    <img
+                      src="https://img.freepik.com/free-vector/contact-us-button_78370-3753.jpg"
+                      alt="contact"
+                    />
+                  </LightSpeed>
                 </div>
               </div>
             </div>
             <div className="col-md-6 col-lg-6">
-              <div className="card2 d-flex card border-0 px-4 py-5">
-                <div className="row">
+              <Rotate>
+                <div className="card2 d-flex card border-0 px-4 py-5">
                   <div className="row">
-                    <h6 className="icons">
-                      Contact with
-                      <BsLinkedin color="#0077B5" size={30} className="ms-4" />
-                      <BsGithub color="#24292e" size={30} className="ms-4" />
-                      <BsInstagram color="#962fbf" size={30} className="ms-4" />
-                      <BsMedium color="#24292e" size={30} className="ms-4" />
-                    </h6>
-                  </div>
+                    <div className="row">
+                      <h6 className="icons">
+                        Contact with
+                        <BsLinkedin
+                          color="#0077B5"
+                          size={30}
+                          className="ms-4"
+                        />
+                        <BsGithub color="#24292e" size={30} className="ms-4" />
+                        <BsInstagram
+                          color="#962fbf"
+                          size={30}
+                          className="ms-4"
+                        />
+                        <BsMedium color="#24292e" size={30} className="ms-4" />
+                      </h6>
+                    </div>
 
-                  <div className="row px-3 mb-4">
-                    <div className="line" />
-                    <small className="or text-center">OR</small>
-                    <div className="line" />
-                  </div>
-                  <div className="row px-3">
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Enter your Name"
-                      className="mb-3"
-                    />
-                  </div>
-                  <div className="row px-3">
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter your Email"
-                      className="mb-3"
-                    />
-                  </div>
-                  <div className="row px-3">
-                    <textarea
-                      type="text"
-                      name="msg"
-                      placeholder="Enter your Message"
-                      className="mb-3"
-                    />
-                  </div>
-                  <div className="row px-3">
-                    <button className="button" type="submit">
-                      Send Message
-                    </button>
+                    <div className="row px-3 mb-4">
+                      <div className="line" />
+                      <small className="or text-center">OR</small>
+                      <div className="line" />
+                    </div>
+                    <div className="row px-3">
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your Name"
+                        className="mb-3"
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter your Email"
+                        className="mb-3"
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <textarea
+                        type="text"
+                        name="msg"
+                        placeholder="Enter your Message"
+                        className="mb-3"
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <button className="button" type="submit">
+                        Send Message
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Rotate>
             </div>
           </div>
         </div>
